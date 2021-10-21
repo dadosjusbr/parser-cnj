a8e5fce744ffb2ea3096e410cf7d0006aded40e5
marcos-vas <marcosvaas@gmail.com>
2021-10-21 09:56:36 -0300

    tests: testes do parser e planilhas para o teste

--- src/parser_test.py
+++ src/parser_test.py
@@ -2,6 +2,7 @@ from parser_cnj import parse
 import unittest
 import json
 from google.protobuf.json_format import MessageToDict
+from data import Data
 
 
 class TestParser(unittest.TestCase):
@@ -9,15 +10,16 @@ class TestParser(unittest.TestCase):
     def test_jan_2018(self):
         self.maxDiff = None
         # Json com a saida esperada
-        with open('./output_test/expected.json', 'r') as fp:
+        with open('./output_test/test_parser/expected.json', 'r') as fp:
             expected = json.load(fp)
 
-        files = ['./output_test/TJRJ-contracheque.xlsx',
-                 './output_test/TJRJ-direitos-eventuais.xlsx',
-                 './output_test/TJRJ-direitos-pessoais.xlsx',
-                 './output_test/TJRJ-indenizações.xlsx']
-
-        result_data = parse(files, 'tjrj/01/2018')
+        files = ['output_test/test_parser/TJRJ-contracheque.xlsx',
+                 'output_test/test_parser/TJRJ-direitos-eventuais.xlsx',
+                 'output_test/test_parser/TJRJ-direitos-pessoais.xlsx',
+                 'output_test/test_parser/TJRJ-indenizações.xlsx']
+        
+        dados = Data(files)
+        result_data = parse(dados, 'tjrj/01/2018')
         # Converto o resultado do parser, em dict
         result_to_dict = MessageToDict(result_data)
         self.assertEqual(expected, result_to_dict)
