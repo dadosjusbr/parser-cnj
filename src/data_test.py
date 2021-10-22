a50f19504a6bc67c3579704d4e314cc1fbaa53ab
marcos-vas <marcosvaas@gmail.com>
2021-10-22 08:16:00 -0300

    fix: pegando code, enão uma mensagem

--- src/data_test.py
+++ src/data_test.py
@@ -2,9 +2,8 @@ from data import Data
 import unittest
 
 
-class TestParser(unittest.TestCase):
-    def test_exit_number_lines(self):
-        expected = "Error! The number of lines, is less than the minimum number of 10 lines"
+class TestData(unittest.TestCase):
+    def test_validate(self):
         file_names = ['./output_test/test_data/TJPI-contracheque.xlsx',
                 './output_test/test_data/TJPI-direitos-pessoais.xlsx',
                 './output_test/test_data/TJPI-indenizações.xlsx',
@@ -12,8 +11,8 @@ class TestParser(unittest.TestCase):
 
         with self.assertRaises(SystemExit) as cm:
             dados = Data(file_names)
-            dados.validate
-        self.assertEqual(cm.exception.code, expected)
+            dados.validate()
+        self.assertEqual(cm.exception.code, 1)
         
 
 if __name__ == '__main__':
