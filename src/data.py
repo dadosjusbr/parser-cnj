449eccc757a7f6fae2ef580d09bda46788bfead2
Marcos Vas <marcosvaas@gmail.com>
2021-10-25 08:03:13 -0300

    fix: separa classe
    Co-authored-by: Daniel Fireman <danielfireman@gmail.com>

--- src/data.py
+++ src/data.py
@@ -3,17 +3,39 @@ import sys
 import os
 
 
+    def __read(file):
+        try:
+            data = pd.read_excel(file, engine='openpyxl').to_numpy()
+        except Exception as excep:
+            print(f"Erro lendo as planilhas: {excep}", file=sys.stderr)
+            sys.exit(1)
+
+        return data
+        
+def load(file_names)
+    """Carrega os arquivos passados como parâmetros.
+    
+    :param file_names: slice contendo os arquivos baixados pelo coletor. Os nomes dos arquivos devem seguir uma convenção e começar com contracheque, indenizações, direitos-eventuais e direitos_pessoais.
+    :return um objeto Data() pronto para operar com os arquivos
+    """
+    d = Data()
+    for file in file_names:
+        if 'contracheque' in file:
+            data.contracheque = __read(file)
+        if 'indenizações' in file:
+            data.indenizacoes = __read(file)
+        if 'direitos-eventuais' in file:
+            data.direitos_eventuais = __read(file)
+        if 'direitos-pessoais' in file:
+            data.direitos_pessoais = __read(file)
+    return data
+ 
 class Data:
-    def __init__(self, file_names):
-        for file in file_names:
-            if 'contracheque' in file:
-                self.contracheque = self.__read(file)
-            if 'indenizações' in file:
-                self.indenizacoes = self.__read(file)
-            if 'direitos-eventuais' in file:
-                self.direitos_eventuais = self.__read(file)
-            if 'direitos-pessoais' in file:
-                self.direitos_pessoais = self.__read(file)
+    def __init__(self, contracheque, indenizacoes, direitos_eventuais, direitos_pessoais):
+        self.contracheque = contracheque
+        self.indenizacoes = indenizacoes
+        self.direitos_eventuais = direitos_eventuais
+        self.direitos_pessoais = direitos_pessoais
 
     def validate(self):
         MIN_ROWS = 10
