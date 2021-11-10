5c4cb04cbdbdda8237577a4f712fadef233f99ce
marcos-vas <marcosvaas@gmail.com>
2021-11-10 10:11:38 -0300

    refactor: separa arquivos, para ficar mais limpo

---
+++ src/headers_keys.py
@@ -0,0 +1,46 @@
+CONTRACHEQUE = "contracheque"
+INDENIZACOES = "indenizações"
+DIREITOS_EVENTUAIS = "direitos-eventuais"
+DIREITOS_PESSOAIS = "direitos-pessoais"
+
+HEADERS = {
+    CONTRACHEQUE: {
+        "Subsídio": 3,
+        "Previdência Pública": 8,
+        "Imposto de renda": 9,
+        "Descontos Diversos": 10,
+        "Retenção por Teto Constitucional": 11,
+        "Remuneração do órgão de origem ": 14,
+        "Diárias": 15,
+    },
+    INDENIZACOES: {
+        "Auxílio-alimentação": 3,
+        "Auxílio Pré-escolar": 4,
+        "Auxílio Saúde": 5,
+        "Auxílio Natalidade": 6,
+        "Auxílio Moradia": 7,
+        "Ajuda de Custo": 8,
+        "Outra 1": 9,
+        "Detalhe 1": 10,
+        "Outra 2": 11,
+        "Detalhe 2": 12,
+        "Outra 3": 13,
+        "Detalhe 3": 14,
+    },
+    DIREITOS_EVENTUAIS: {
+        "Abono constitucional de 1/3 de férias": 3,
+        "Indenização de férias": 4,
+        "Antecipação de férias": 5,
+        "Gratificação natalina": 6,
+        "Antecipação de gratificação natalina": 7,
+        "Substituição": 8,
+        "Gratificação por exercício cumulativo": 9,
+        "Gratificação por encargo Curso/Concurso": 10,
+        "Pagamentos retroativos": 11,
+        "JETON": 12,
+        "Outra 1": 13,
+        "Detalhe 1": 14,
+        "Outra 2": 15,
+        "Detalhe 2": 16,
+    },
+}
---
+++ src/methods_table.py
@@ -0,0 +1,16 @@
+def isNaN(string):
+    return string != string
+
+def to_number(element):
+    # A value was found with incorrect formatting. (3,045.99 instead of 3045.99)
+    if isNaN(element):
+        return 0.0
+    if type(element) == str:
+        if "." in element and "," in element:
+            element = element.replace(".", "").replace(",", ".")
+        elif "," in element:
+            element = element.replace(",", ".")
+        elif "-" in element:
+            element = 0.0
+
+    return float(element)
