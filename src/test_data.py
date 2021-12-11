812ffd065b49492c847825fac9bb257401b21d67
Daniel Fireman <danielfireman@gmail.com>
2021-12-11 16:32:39 +0000

    corrigindo testes para rodar no CI

--- src/test_data.py
+++ src/test_data.py
@@ -5,11 +5,11 @@ import unittest
 class TestData(unittest.TestCase):
     def test_validate_existence(self):
         files = [
-            "output_test/test_data/contracheque-tjpi-2019-03.xlsx",
-            "output_test/test_data/direitos-pessoais-tjpi-2019-03.xlsx",
-            "output_test/test_data/indenizacoes-tjpi-2019-03.xlsx",
-            "output_test/test_data/direitos-eventuais-tjpi-2019-03.xlsx",
-            "output_test/test_data/controle-de-arquivos-tjrr-2019-09.xlsx",
+            "src/output_test/test_data/contracheque-tjpi-2019-03.xlsx",
+            "src/output_test/test_data/direitos-pessoais-tjpi-2019-03.xlsx",
+            "src/output_test/test_data/indenizacoes-tjpi-2019-03.xlsx",
+            "src/output_test/test_data/direitos-eventuais-tjpi-2019-03.xlsx",
+            "src/output_test/test_data/controle-de-arquivos-tjrr-2019-09.xlsx",
         ]
 
         with self.assertRaises(SystemExit) as cm:
@@ -19,22 +19,22 @@ class TestData(unittest.TestCase):
 
     def test_validate_zeroless(self):
         files = [
-            "output_test/test_data/contracheque-tjpe-2019-05.xlsx",
-            "output_test/test_data/direitos-pessoais-tjpe-2019-05.xlsx",
-            "output_test/test_data/indenizacoes-tjpe-2019-05.xlsx",
-            "output_test/test_data/direitos-eventuais-tjpe-2019-05.xlsx",
-            "output_test/test_data/controle-de-arquivos-tjpe-2019-05.xlsx",
+            "src/output_test/test_data/contracheque-tjpe-2019-05.xlsx",
+            "src/output_test/test_data/direitos-pessoais-tjpe-2019-05.xlsx",
+            "src/output_test/test_data/indenizacoes-tjpe-2019-05.xlsx",
+            "src/output_test/test_data/direitos-eventuais-tjpe-2019-05.xlsx",
+            "src/output_test/test_data/controle-de-arquivos-tjpe-2019-05.xlsx",
         ]
         dados = data.load(files, "2019", "05", "TJPE")
         dados.validate()
 
     def test_validate_summary(self):
         files = [
-            "output_test/test_data/contracheque-tjba-2019-06.xlsx",
-            "output_test/test_data/direitos-pessoais-tjba-2019-06.xlsx",
-            "output_test/test_data/indenizacoes-tjba-2019-06.xlsx",
-            "output_test/test_data/direitos-eventuais-tjba-2019-06.xlsx",
-            "output_test/test_data/controle-de-arquivos-tjba-2019-06.xlsx",
+            "src/output_test/test_data/contracheque-tjba-2019-06.xlsx",
+            "src/output_test/test_data/direitos-pessoais-tjba-2019-06.xlsx",
+            "src/output_test/test_data/indenizacoes-tjba-2019-06.xlsx",
+            "src/output_test/test_data/direitos-eventuais-tjba-2019-06.xlsx",
+            "src/output_test/test_data/controle-de-arquivos-tjba-2019-06.xlsx",
         ]
         with self.assertRaises(SystemExit) as cm:
             dados = data.load(files, "2019", "06", "TJBA")
--- src/test_parser.py
+++ src/test_parser.py
@@ -11,14 +11,14 @@ class TestParser(unittest.TestCase):
     def test_jan_2018(self):
         self.maxDiff = None
         # Json com a saida esperada
-        with open('./output_test/test_parser/expected.json', 'r') as fp:
+        with open('src/output_test/test_parser/expected.json', 'r') as fp:
             expected = json.load(fp)
 
-        files = ['output_test/test_parser/contracheque-tjrj-2018-01.xlsx',
-                 'output_test/test_parser/direitos-eventuais-tjrj-2018-01.xlsx',
-                 'output_test/test_parser/direitos-pessoais-tjrj-2018-01.xlsx',
-                 'output_test/test_parser/indenizacoes-tjrj-2018-01.xlsx',
-                 'output_test/test_parser/controle-de-arquivos-tjrj-2018-01.xlsx']
+        files = ['src/output_test/test_parser/contracheque-tjrj-2018-01.xlsx',
+                 'src/output_test/test_parser/direitos-eventuais-tjrj-2018-01.xlsx',
+                 'src/output_test/test_parser/direitos-pessoais-tjrj-2018-01.xlsx',
+                 'src/output_test/test_parser/indenizacoes-tjrj-2018-01.xlsx',
+                 'src/output_test/test_parser/controle-de-arquivos-tjrj-2018-01.xlsx']
                  
         dados = load(files, '2018', '01', 'TJRJ')
         result_data = parse(dados, 'tjrj/01/2018')
@@ -31,14 +31,14 @@ class TestParser(unittest.TestCase):
     def test_spreadsheet_with_one_line(self):
         self.maxDiff = None
         # Json com a saida esperada
-        with open('./output_test/test_parser/test_one_line/expected.json', 'r') as fp:
+        with open('src/output_test/test_parser/test_one_line/expected.json', 'r') as fp:
             expected = json.load(fp)
 
-        files = ['output_test/test_parser/test_one_line/contracheque-tjpi-2020-01.xlsx',
-                'output_test/test_parser/test_one_line/direitos-eventuais-tjpi-2020-01.xlsx',
-                'output_test/test_parser/test_one_line/direitos-pessoais-tjpi-2020-01.xlsx',
-                'output_test/test_parser/test_one_line/indenizacoes-tjpi-2020-01.xlsx',
-                'output_test/test_parser/test_one_line/controle-de-arquivos-tjpi-2020-01.xlsx']
+        files = ['src/output_test/test_parser/test_one_line/contracheque-tjpi-2020-01.xlsx',
+                'src/output_test/test_parser/test_one_line/direitos-eventuais-tjpi-2020-01.xlsx',
+                'src/output_test/test_parser/test_one_line/direitos-pessoais-tjpi-2020-01.xlsx',
+                'src/output_test/test_parser/test_one_line/indenizacoes-tjpi-2020-01.xlsx',
+                'src/output_test/test_parser/test_one_line/controle-de-arquivos-tjpi-2020-01.xlsx']
 
         dados = load(files, '2020', '01', 'TJPI')
         result_data = parse(dados, 'tjpi/01/2020')
